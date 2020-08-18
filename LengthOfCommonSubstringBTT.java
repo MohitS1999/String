b5381832dbceca938c9e305b92f9d44b278bd2ee
MohitS1999 <49729753+MohitS1999@users.noreply.github.com>
2020-08-18 22:15:07 +0530

    Hard

---
+++ LengthOfCommonSubstringBTT.java
@@ -0,0 +1,27 @@
+import java.util.*;
+public class LengthOfCommonSubstringBTT{
+	public static void main(String[] arhs){
+		Scanner sc=new Scanner (System.in);
+		int t=sc.nextInt();
+		while (t-->0){
+			String x=sc.next();
+			String y=sc.next();
+			System.out.println(commonSubstring(x,y,x.length(),y.length()));
+		}
+	}
+	public static int commonSubstring(String x,String y,int n,int m){
+		int dp[][]=new int[n+1][m+1];
+		int res=0;
+		for (int i=0;i<n;i++){
+			for (int j=0;j<m;j++){
+				if (x.charAt(i)==y.charAt(j))
+				{
+					dp[i+1][j+1]=dp[i][j]+1;
+					res=Math.max(res,dp[i+1][j+1]);
+				}
+			}
+		}
+		return res;
+	}
+	
+}
