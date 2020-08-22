8dc2b35c988ec61c75fc76254e12ee3ad6fc9e96
MohitS1999 <49729753+MohitS1999@users.noreply.github.com>
2020-08-22 23:06:09 +0530

    Hard
    Top to Bottom

---
+++ LengthOfLongestPalindromeSubsequenceTTB.java
@@ -0,0 +1,29 @@
+import java.util.*;
+public class LengthOfLongestPalindromeSubsequenceTTB{
+	public static void main(String[] args){
+		Scanner sc=new Scanner (System.in);
+		int t=sc.nextInt();
+		while (t-->0){
+			String a=sc.next();
+			StringBuffer sb=new StringBuffer();
+			sb.append(a);
+			sb.reverse();
+			int m=a.length();
+			int dp[][]=new int[m+1][m+1];
+			System.out.println(palindromeSub(a,sb.toString(),m,m,dp));
+		}
+	}
+	public static int palindromeSub(String a,String b,int m,int n,int dp[][]){
+		// Base Condition
+		if (m==0 || n==0) return 0;
+		//Memorization
+		if (dp[m][n]!=0) return dp[m][n];
+		
+		if (a.charAt(m-1)==b.charAt(n-1)){
+			return dp[m][n]=palindromeSub(a,b,m-1,n-1,dp)+1;
+		}else{
+			return dp[m][n]=Math.max(palindromeSub(a,b,m-1,n,dp),palindromeSub(a,b,m,n-1,dp));
+		}
+		
+	}
+}
