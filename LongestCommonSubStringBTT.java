bf27f220cbbff7ee32065dc0952e210e6687a17c
MohitS1999 <49729753+MohitS1999@users.noreply.github.com>
2020-08-26 22:30:08 +0530

    Medium
    Bottom to top

---
+++ LongestCommonSubStringBTT.java
@@ -0,0 +1,40 @@
+import java.util.*;
+public class LongestCommonSubStringBTT{
+	public static void main(String[] args){
+		Scanner sc=new Scanner (System.in);
+		int t=sc.nextInt();
+		while (t-->0){
+			String a=sc.next();
+			String b=sc.next();	
+			System.out.println(commonSubstring(a,b));
+		}
+	}
+	public static String commonSubstring(String a,String b){
+		int m=a.length();
+		int n=b.length();
+		int dp[][]=new int[m+1][n+1];
+		int indexI=0;
+		int indexJ=0;
+		int max=0;
+		for (int i=1;i<=m;i++){
+			for (int j=1;j<=n;j++){
+				if (a.charAt(i-1)==b.charAt(j-1)){
+					dp[i][j]=dp[i-1][j-1]+1;
+					if (max<dp[i][j]){
+						max=dp[i][j];
+						indexI=i;
+						indexJ=j;
+					}
+				}
+				else dp[i][j]=0;
+			}
+		}
+		String s="";
+		while (dp[indexI][indexJ]!=0){
+			s=a.charAt(indexI-1)+s;
+			indexI--;
+			indexJ--;
+		}
+		return s;
+	} 
+}
