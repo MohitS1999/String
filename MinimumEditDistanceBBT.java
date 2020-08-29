816c673ed0e149007b183db5ee5ada58c83ac4cf
MohitS1999 <49729753+MohitS1999@users.noreply.github.com>
2020-08-29 22:44:41 +0530

    Medium
    Hard

--- MinimumEditDistanceBBT.java
+++ MinimumEditDistanceBBT.java
@@ -16,6 +16,7 @@ public class MinimumEditDistanceBBT{
 					if (i>0 && j==0)
 						dp[i][j]=i;
 				}else{
+					//if both character same then we will not do any changes in string
 					if (a.charAt(i-1)==b.charAt(j-1)){
 						dp[i][j]=dp[i-1][j-1];
 					}else{
