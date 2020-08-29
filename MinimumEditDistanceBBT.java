e0bea89a46ac67a417854b671bdf9d9416f852bf
MohitS1999 <49729753+MohitS1999@users.noreply.github.com>
2020-08-29 22:43:02 +0530

    Medium

---
+++ MinimumEditDistanceBBT.java
@@ -0,0 +1,29 @@
+import java.util.*;
+public class MinimumEditDistanceBBT{
+	public static void main(String[] arhs){
+		Scanner sc=new Scanner (System.in);
+		String a=sc.next();
+		String b=sc.next();
+		System.out.println(minEditDis(a,b,a.length(),b.length()));
+	}
+	public static int minEditDis(String a,String b ,int m,int n){
+		int dp[][]=new int[m+1][n+1];
+		for (int i=0;i<=m;i++){
+			for (int j=0;j<=n;j++){
+				if (i==0 || j==0){
+					if (i==0 && j>0)
+						dp[i][j]=j;
+					if (i>0 && j==0)
+						dp[i][j]=i;
+				}else{
+					if (a.charAt(i-1)==b.charAt(j-1)){
+						dp[i][j]=dp[i-1][j-1];
+					}else{
+						dp[i][j]=Math.min(dp[i-1][j-1],Math.min(dp[i-1][j],dp[i][j-1]))+1;
+					}
+				}
+			}
+		}
+		return dp[m][n];
+	}
+}
