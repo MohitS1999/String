f54f86c87889b1a0ddd4f30484a3124291850b3a
MohitS1999 <49729753+MohitS1999@users.noreply.github.com>
2020-08-31 21:55:27 +0530

    Medium

---
+++ MinimumEditDistanceTTB.java
@@ -0,0 +1,22 @@
+import java.util.*;
+public class MinimumEditDistanceTTB{
+	public static void main(String[] args){
+		Scanner sc=new Scanner (System.in);
+		String a=sc.next();
+		String b=sc.next();
+		int m=a.length();
+		int n=b.length();
+		int dp[][]=new int[m+1][n+1];
+		System.out.println(minEditDis(a,b,m,n,dp));
+	}
+	public static int minEditDis(String a,String b,int m,int n,int[][] dp){
+		if (m==0 && n==0) return 0;
+		if (m==0 && n>0) return n;
+		if (m>0 && n==0) return m;
+		if (dp[m][n]!=0) return dp[m][n];
+		if (a.charAt(m-1)==b.charAt(n-1)){
+			return dp[m][n]=minEditDis(a,b,m-1,n-1,dp);
+		}
+		return dp[m][n]=Math.min(minEditDis(a,b,m-1,n-1,dp),Math.min(minEditDis(a,b,m-1,n,dp),minEditDis(a,b,m,n-1,dp)))+1;
+	} 
+}
