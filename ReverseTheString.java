34a75c77d3b91c075061406af15b280f6f7f8ff4
MohitS1999 <49729753+MohitS1999@users.noreply.github.com>
2020-09-09 22:02:38 +0530

    Easy

--- ReverseTheString.java
+++ ReverseTheString.java
@@ -10,9 +10,12 @@ public class ReverseTheString{
 			int i=0;
 			int n=s.length();
 			while (i<n){
+				//check in strting there is space or not
 				while (i<n && s.charAt(i)==' ') i++;
+				//if the i > length of string then break the loop
 				if (i>=n) break;
 				int j=i+1;
+				//space after the string
 				while (j<n && s.charAt(j)!=' ') j++;
 				String sub=s.substring(i,j);
 				res=(res.length()==0) ? sub : sub+" "+res;
