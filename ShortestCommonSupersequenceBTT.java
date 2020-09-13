065fd865957813cf64121ef2d1c76fa3a0c5dd47
MohitS1999 <49729753+MohitS1999@users.noreply.github.com>
2020-09-13 22:27:30 +0530

    Hard

--- ShortestCommonSupersequenceBTT.java
+++ ShortestCommonSupersequenceBTT.java
@@ -12,6 +12,7 @@ public class ShortestCommonSupersequenceBTT{
 	public static String shortestsubseq(String a,String b){
 		int m=a.length();
 		int n=b.length();
+		//LCS
 		int dp[][]=new int[m+1][n+1];
 		for (int i=1;i<=m;i++){
 			for (int j=1;j<=n;j++){
@@ -24,6 +25,7 @@ public class ShortestCommonSupersequenceBTT{
 		int i=m;
 		int j=n;
 		String s="";
+		//make a string
 		while (i!=0 && j!=0){
 			if (a.charAt(i-1)==b.charAt(j-1)){
 				s=a.charAt(i-1)+s;
